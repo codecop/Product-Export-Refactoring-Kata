90b51ec350a73356e09530e9a2d08731d428bd3d
Peter Kofler <peter.kofler@code-cop.org>
2020-04-24 17:41:49 +0200

    Remove struct from header, it is incomplete.

--- C/Product.h
+++ C/Product.h
@@ -3,13 +3,6 @@
 #include "ModelObject.h"
 #include "Price.h"
 
-struct Product {
-    struct ModelObject base;
-    char* name;
-    long weight;
-    struct Price* price;
-};
-
 extern struct Product* makeProduct(char* name, char* id, long weight, struct Price* price);
 extern char* getProductName(struct Product* this);
 extern char* getProductId(struct Product* this);
