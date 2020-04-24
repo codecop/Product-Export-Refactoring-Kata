5e4f49641f067c7a2999034a29703cab85c91f74
Peter Kofler <peter.kofler@code-cop.org>
2020-04-24 17:12:36 +0200

    Move conversion script forward.

--- C/j/_java2c.rb
+++ C/j/_java2c.rb
@@ -1,6 +1,6 @@
 # Make sure the Java code uses THIS for fields.
 # Use Cleanup "use this" before converting code.
-@@classname = ""
+@@classname = ''
 
 def struct
   "struct #{@@classname}"
@@ -13,30 +13,36 @@ def convert_line(line)
   end
 
   typeName = /[A-Z]\w*/
-  type = /(?:#{typeName}|boolean|int|void|double|float|char\*)(?:\[\])?/
+  type = /(?:#{typeName}|boolean|int|long|void|float|double|char\*)(?:\[\])?/
   fieldName = /[a-z]\w*/
 
   line = line.
     # remove
-    sub(/^package (.*);/, ""). # package statement
-    sub(/^import (.*);/, "").
-    sub(/@Override/, "").
-    sub(/@SuppressWarnings\("unchecked"\)/, "").
-    # map
-    gsub(/String /, "char* ").
+    sub(/^package (.*);/, ''). # package statement
+    sub(/^import (.*);/, ''). # imports
+    sub(/@Override/, ''). # annotations
+    sub(/@SuppressWarnings\("[^"]+"\)/, '').
+    # map types
+    gsub(/String /, 'char* ').
+    gsub(/boolean /, 'bool ').
+    # map fields
+    gsub(/(?:private|protected) (?:final )?(#{type}) (#{fieldName})/, "\\1 \\2").
+    # map class struct
     sub(/public class (#{typeName}) implements (#{typeName}) \{/, "struct \\1 {\n\t\\2 base;").
     sub(/public class (#{typeName}) \{/, "struct \\1 {").
-    gsub(/(?:private|protected) (?:final )?(#{type}) (#{fieldName})/, "\\1 \\2"). # fields
-
+    gsub(/^\}\n/, ''). # remove end of class
+    # map declarations
     sub(/public #{@@classname}\(([^)]*)\) \{/, # constructor declaration
-      "};\n #{struct}* make#{@@classname}(\\1) {\n" +
+      "};\n\n" + # end of struct
+      "  #{struct}* make#{@@classname}(\\1) {\n" +
       "  #{struct} *this = (#{struct} *)malloc(sizeof(#{struct}));").
-    # add return this;
+    # add return this; - found by compile error
+    # add this
     gsub(/public (#{type}) (#{fieldName})\(([^)]*)\) \{/, "\\1 \\2(#{struct} *this, \\3) {"). # this
-    gsub(/, \)/, ")").
-    gsub(/throw new UnsupportedOperationException\(("[^"]+")\);/, "printf(\"Unsupported Operation %s\\n\", \\1); exit(1);").
-    gsub(/^\}\n/, ""). # remove end of class
-    gsub(/this\./, "this->") # fix .
+    gsub(/, \)/, ')'). # fix arguments introduced by this
+    gsub(/this\./, 'this->'). # fix .
+    # map single statements
+    gsub(/throw new UnsupportedOperationException\(("[^"]+")\);/, 'printf("Unsupported Operation %s\\n", \\1); exit(1);').
 
 #    sub(/System\.out\.println/, "console.log"). # println
 #    sub(/(public|private) (#{type}) (\w+\([^)]*\))/, "\\1 \\3: \\2"). # method return type order
@@ -53,16 +59,25 @@ def convert_line(line)
 #    # map operators
 #    gsub(/ == /, " === ").
 #    gsub(/ != /, " !== ")
+    sub(/foobarbaz/, 'foobarbaz') # fake last statement
 
   if line =~ /(?:: |extends |implements |new )(#{type})/
     @@used_types << $1
+  elsif line =~ /(#{type}) #{fieldName}/
+    @@used_types << $1
+  elsif line =~ /malloc\(/
+    @@used_types << 'stdlib.h'
+  elsif line =~ /bool /
+    @@used_types << 'stdbool.h'
+  elsif line =~ /printf/
+    @@used_types << 'stdio.h'
   end
 
   line.rstrip
 end
 
 def convert_source(lines)
-  @@classname = ""
+  @@classname = ''
   @@used_types = []
   code = lines.map { |line| convert_line(line) }
 
@@ -70,7 +85,7 @@ def convert_source(lines)
     find_all { |t| t =~ /^[A-Z]/ }.
     reject { |t| t == 'Random' }.
     sort.uniq.
-    map { |t| "#include \"#{to_c_file_name(t)}.h\";" }
+    map { |t| "#include \"#{to_c_file_name(t)}.h\"" }
 
   imports + code
 end
@@ -86,9 +101,9 @@ def to_c_file_name(java_name)
   java_name
 end
 
-Dir["Price.java"].each do |java_file|
+Dir['Product.java'].each do |java_file|
   java_lines = IO.readlines(java_file)
   c_lines = convert_source(java_lines)
-  c_file = to_c_file_name(java_file[/^[^.]+/]) + ".c"
+  c_file = to_c_file_name(java_file[/^[^.]+/]) + '.c'
   save(c_file, c_lines)
 end
