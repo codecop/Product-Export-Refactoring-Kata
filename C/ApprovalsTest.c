34883b5be3a747283f2c92af7084d5a3aeb61beb
Peter Kofler <peter.kofler@code-cop.org>
2020-05-03 23:54:43 +0200

    Remame tests.

--- C/ApprovalsTest.c
+++ C/ApprovalsTest.c
@@ -20,10 +20,10 @@ static void test_approvals_save_load(void** state)
     (void)state; /* unused */
 
     const char* s = "abc123";
-    approvals_save("foo", s);
-    const char* r = approvals_load("foo");
+    approvals_save("test_approvals_save_load", s);
+    const char* r = approvals_load("test_approvals_save_load");
     assert_string_equal(s, r);
-    approvals_delete("foo");
+    approvals_delete("test_approvals_save_load");
 }
 
 static void test_approvals_verify(void** state)
@@ -31,15 +31,15 @@ static void test_approvals_verify(void** state)
     (void)state; /* unused */
 
     const char* approved =
-        approvals_verify("abc123", __FILE__, "test_approval", "txt");
+        approvals_verify("abc123", __FILE__, "test_approvals_verify", "txt");
     assert_string_equal(approved, "abc123");
 }
 
-static void test_verify(void** state)
+static void test_verify_xml(void** state)
 {
     (void)state; /* unused */
 
-    verify_xml("<nope />", "test_verify");
+    verify_xml("<nope />", "test_verify_xml");
 }
 
 int main(void)
@@ -48,7 +48,7 @@ int main(void)
         cmocka_unit_test(test_approvals_name),      /* */
         cmocka_unit_test(test_approvals_save_load), /* */
         cmocka_unit_test(test_approvals_verify),    /* */
-        cmocka_unit_test(test_verify),              /* */
+        cmocka_unit_test(test_verify_xml),          /* */
     };
 
     return cmocka_run_group_tests(test_suite, NULL, NULL);
