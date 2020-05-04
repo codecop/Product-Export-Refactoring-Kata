9ac2b7bbbe7e414a7e18cc561e4698c8eed9de59
Peter Kofler <peter.kofler@code-cop.org>
2020-05-04 23:17:56 +0200

    Simplify, account for null byte at end.

--- C/StringBuilder.c
+++ C/StringBuilder.c
@@ -20,14 +20,15 @@ struct StringBuilder* make_sb(void)
     return this;
 }
 
-static void sb_ensure_size(struct StringBuilder* this, size_t size)
+static void sb_ensure_size(struct StringBuilder* this, size_t length)
 {
+    size_t size = this->position + length + 1;
     if (this->buffer_length > size) {
         return;
     }
 
     char* old_buffer = this->buffer;
-    while (this->buffer_length <= size) {
+    while (this->buffer_length < size) {
         this->buffer_length *= 2;
     }
     this->buffer = malloc(this->buffer_length);
@@ -38,23 +39,21 @@ static void sb_ensure_size(struct StringBuilder* this, size_t size)
 void sb_append(struct StringBuilder* this, const char* s)
 {
     size_t length = strlen(s);
-    sb_ensure_size(this, this->position + length);
+    sb_ensure_size(this, length);
     memcpy(this->buffer + this->position, s, length + 1);
     this->position += length;
 }
 
 void sb_append_long(struct StringBuilder* this, long l)
 {
-    size_t length = 10;
-    sb_ensure_size(this, this->position + length);
+    sb_ensure_size(this, 10);
     int written = sprintf(this->buffer + this->position, "%lu", l);
     this->position += written;
 }
 
 void sb_append_double(struct StringBuilder* this, double d)
 {
-    size_t length = 24;
-    sb_ensure_size(this, this->position + length);
+    sb_ensure_size(this, 24);
     int written = sprintf(this->buffer + this->position, "%f", d);
     this->position += written;
 }
