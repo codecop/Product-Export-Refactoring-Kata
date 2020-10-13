781a73ee97decc62e0d4ce1bfe9012c16de5ccf0
Peter Kofler <peter.kofler@code-cop.org>
2020-10-13 18:42:21 +0200

    Have a single product to XML but it has arguments, sigh.

--- C/XMLExporter.c
+++ C/XMLExporter.c
@@ -21,10 +21,12 @@ struct StringBuilder* make_xml()
 }
 
 #define PRODUCT_DETAIL_NONE 0
-#define PRODUCT_DETAIL_DETAILS 1
-#define PRODUCT_DETAIL_PRICE 2
+#define PRODUCT_DETAIL_STYLIST 1
+#define PRODUCT_DETAIL_WEIGHT 2
+#define PRODUCT_DETAIL_PRICE 4
+#define PRODUCT_DETAIL_LOCATION 8
 
-void xml_product(struct StringBuilder* xml, const struct Product* product, unsigned int details)
+void xml_product(struct StringBuilder* xml, const struct Product* product, struct Store* store, unsigned int details)
 {
     sb_append(xml, "<product");
 
@@ -32,13 +34,23 @@ void xml_product(struct StringBuilder* xml, const struct Product* product, unsig
     sb_append(xml, get_product_id(product));
     sb_append(xml, "'");
 
-    if (details & PRODUCT_DETAIL_DETAILS) {
+    if (details & PRODUCT_DETAIL_STYLIST) {
         if (is_product_event(product)) {
             sb_append(xml, " stylist='");
             sb_append(xml, stylist_for(product));
             sb_append(xml, "'");
         }
+    }
 
+    if (details & PRODUCT_DETAIL_LOCATION) {
+        if (is_product_event(product)) {
+            sb_append(xml, " location='");
+            sb_append(xml, get_store_name(store));
+            sb_append(xml, "'");
+        }
+    }
+
+    if (details & PRODUCT_DETAIL_WEIGHT) {
         if (get_product_weight(product) > 0) {
             sb_append(xml, " weight='");
             sb_append_long(xml, get_product_weight(product));
@@ -77,7 +89,7 @@ const char* xml_export_full(const struct LinkedList* orders)
         const struct LinkedList* products = get_order_products(order);
         for (const struct LinkedList* node = products; node; node = node->next) {
             const struct Product* product = (const struct Product*)node->data;
-            xml_product(xml, product, PRODUCT_DETAIL_DETAILS | PRODUCT_DETAIL_PRICE);
+            xml_product(xml, product, NULL, PRODUCT_DETAIL_STYLIST | PRODUCT_DETAIL_WEIGHT | PRODUCT_DETAIL_PRICE);
         }
 
         sb_append(xml, "</order>");
@@ -104,13 +116,7 @@ const char* xml_export_tax_details(struct LinkedList* orders)
         const struct LinkedList* products = get_order_products(order);
         for (const struct LinkedList* node = products; node; node = node->next) {
             const struct Product* product = (const struct Product*)node->data;
-            sb_append(xml, "<product");
-            sb_append(xml, " id='");
-            sb_append(xml, get_product_id(product));
-            sb_append(xml, "'");
-            sb_append(xml, ">");
-            sb_append(xml, get_product_name(product));
-            sb_append(xml, "</product>");
+            xml_product(xml, product, NULL, PRODUCT_DETAIL_NONE);
             if (is_product_event(product))
                 tax += get_price_amount_in_currency(get_product_price(product), "USD") * 0.25;
             else
@@ -156,32 +162,7 @@ const char* xml_export_store(struct Store* store)
     const struct LinkedList* products = get_store_stock(store);
     for (const struct LinkedList* node = products; node; node = node->next) {
         const struct Product* product = (const struct Product*)node->data;
-        sb_append(xml, "<product");
-        sb_append(xml, " id='");
-        sb_append(xml, get_product_id(product));
-        sb_append(xml, "'");
-        if (is_product_event(product)) {
-            sb_append(xml, " location='");
-            sb_append(xml, get_store_name(store));
-            sb_append(xml, "'");
-        }
-        else {
-            sb_append(xml, " weight='");
-            sb_append_long(xml, get_product_weight(product));
-            sb_append(xml, "'");
-        }
-
-        sb_append(xml, ">");
-        sb_append(xml, "<price");
-        sb_append(xml, " currency='");
-        sb_append(xml, get_price_currency(get_product_price(product)));
-        sb_append(xml, "'>");
-        const char* formatted = make_formatted_double(get_price_amount(get_product_price(product)));
-        sb_append(xml, formatted);
-        free((void*)formatted);
-        sb_append(xml, "</price>");
-        sb_append(xml, get_product_name(product));
-        sb_append(xml, "</product>");
+        xml_product(xml, product, store, PRODUCT_DETAIL_LOCATION | PRODUCT_DETAIL_WEIGHT | PRODUCT_DETAIL_PRICE);
     }
 
     sb_append(xml, "</store>");
